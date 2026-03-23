9a49ab65b933c1d4cb9975466f7fc5d8b76a3997
Jamleck <tumkibet32@gmail.com>
2026-03-23 12:38:23 +0300

    first commit

---
+++ schoolSystem.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+print("Hello welcome to School Managment system ")
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="jamkei5242",
+    database="school_system"
+)
+
+cursor = conn.cursor()
+
+print("Database connected successfully")
+
+# CreATE A TABLE CCALLED STUDENTS
+cursor.execute("""
+ CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+)
+""")
+
+conn.commit()
+
+print("Tbale created successfully")
+
+
+cursor.execute("SELECT * FROM  school_system")
+for row in cursor:
+    print(row)
